2eebb50fa46fe37afff3e2d9a3bf0108f00a8fed
Felix23 <854342@stud.unive.it>
2019-05-25 15:34:44 +0200

    preparing test for different par_levels + good parameters

--- main.cc
+++ main.cc
@@ -33,10 +33,10 @@ int main(int argc, char* argv[]){
     std::string toy = parent+"toy2.csv";
     std::string gender = parent+"gender.csv";
     std::string ai_example = parent+"ai.data";
-    int N=0, n_runs=3;
+    int N=0, n_runs=10;
     unsigned long n_features = 0;
     unsigned short const d=4;
-    int const max_nr_tables=1;
+    int const max_nr_tables=20;
     srand(23);
     n_threads=1;
     par_value=0;
